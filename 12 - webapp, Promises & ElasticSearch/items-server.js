eaa11ef03aff80529044d3071cb2a8251a7235de
Joao H. Trindade <joao.h.trindade@gmail.com>
2020-12-11 13:07:31 +0000

    WebApp (ElasticSearch)

--- 12 - webapp, Promises & ElasticSearch/items-server.js
+++ 12 - webapp, Promises & ElasticSearch/items-server.js
@@ -1,12 +1,17 @@
 'use strict'
 
+const storage_host = 'localhost'
+const storage_port = 9200
+
 const default_port = 8888
 const port = process.argv[2] || default_port
 
 const express = require('express')
 const app = express()
 
-const storage = require('./items-storage-mem.js')
+const storageCreator = require('./items-storage-db.js')
+const storage = storageCreator(storage_host, storage_port)
+
 const quotes = require('./items-quotes.js')
 
 const serviceCreator = require('./items-service.js')
